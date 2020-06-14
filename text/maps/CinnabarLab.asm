710a2c7b76a39fbba80bf8ad1014b185956d12dd
Nog-Frog <65667411+Nog-Frog@users.noreply.github.com>
2020-06-14 16:03:16 +0300

    Revert "Translated Cinnabar Lab"

--- text/maps/CinnabarLab.asm
+++ text/maps/CinnabarLab.asm
@@ -1,30 +1,29 @@
 _Lab1Text1::
-	text "אנחנו חוקרים פה"
-	line "#ימונים באופן"
-	cont "נרחב."
+	text "We study #MON"
+	line "extensively here."
 
-	para "לעתים קרובות אנשים"
-	line "מביאים אלינו"
-	cont "#ימונים נדירים"
-	cont "לבחינה."
+	para "People often bring"
+	line "us rare #MON"
+	cont "for examination."
 	done
 
 _Lab1Text2::
-	text "תמונה של מייסד"
-	line "ה-LAB, DR.FUJI!" ; TODO
+	text "A photo of the"
+	line "LAB's founder,"
+	cont "DR.FUJI!"
 	done
 
 _Lab1Text3::
-	text "#MON LAB" ; TODO
-	line "חדר ישיבות"
+	text "#MON LAB"
+	line "Meeting Room"
 	done
 
 _Lab1Text4::
-	text "#MON LAB" ; TODO
-	line "חדר מו\"פ"
+	text "#MON LAB"
+	line "R-and-D Room"
 	done
 
 _Lab1Text5::
-	text "#MON LAB" ; TODO
-	line "חדר בדיקות"
+	text "#MON LAB"
+	line "Testing Room"
 	done

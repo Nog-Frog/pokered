fd12f37a1bfd5a4e67c8cd5d497df2a77b1956e6
NoamGeller <noam.geller@mail.huji.ac.il>
2020-07-02 19:06:13 +0300

    Translated Route16FlyHouse

--- text/maps/Route16FlyHouse.asm
+++ text/maps/Route16FlyHouse.asm
@@ -1,31 +1,29 @@
 _Route16HouseText3::
-	text "Oh, you found my"
-	line "secret retreat!"
+	text "אה, מצאת את המקלט"
+	line "הסודי שלי!"
 
-	para "Please don't tell"
-	line "anyone I'm here."
-	cont "I'll make it up"
-	cont "to you with this!"
+	para "בבקשה אל תספר לאף"
+	line "אחד שאני כאן. אפצה"
+	cont "אותך עם זה!"
 	prompt
 
 _ReceivedHM02Text::
-	text "<PLAYER> received"
-	line "HM02!@@"
+	text "<PLAYER> קיבל"
+	line "20MH!@@"
 
 _HM02ExplanationText::
-	text "HM02 is FLY."
-	line "It will take you"
-	cont "back to any town."
+	text "20MH מכיל תעופה."
+	line "הוא ייקח אותך"
+	cont "בחזרה לכל עיר."
 
-	para "Put it to good"
-	line "use!"
+	para "השתמש בו לטובה!"
 	done
 
 _HM02NoRoomText::
-	text "You don't have any"
-	line "room for this."
+	text "אין לך שום מקום"
+	line "בשביל זה."
 	done
 
 _Route16HouseText_1e652::
-	text "FEAROW: Kyueen!"
+	text "פחדרור: קיואין!"
 	done
